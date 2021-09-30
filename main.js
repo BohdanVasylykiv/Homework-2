1efc1689663cfbf831d576edb3a195b888009576
Bohdan <46776799+BohdanVasylykiv@users.noreply.github.com>
2021-09-30 17:10:51 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+let n = parseInt(prompt('Введіть будь ласка число'));
+let m = parseInt(prompt('Введіть будь ласка число'));
+
+if(!Number.isInteger(n) || !Number.isInteger(m)){
+    alert('Спробуйте ще раз')
+    
+}else{
+    let skipPairs = confirm('Пропускати парні?');
+    document.location.reload()
+    let result = null;
+    for(let i = n; i <= m; ++i){
+        if(skipPairs && !(i % 2)){
+            continue;  
+        }
+        result += i;
+};
+alert(result);
+}
