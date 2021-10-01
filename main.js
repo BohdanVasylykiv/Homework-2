89acc1fffa8f501e7aebefa71a6865542c7e9fa0
Bohdan <46776799+BohdanVasylykiv@users.noreply.github.com>
2021-10-01 15:46:45 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,12 @@ let n = parseInt(prompt('Введіть будь ласка число'));
 let m = parseInt(prompt('Введіть будь ласка число'));
 
 if(!Number.isInteger(n) || !Number.isInteger(m)){
-    alert('Спробуйте ще раз')
+    alert('Спробуйте ще раз');
     
-}else
-{
+}else{
     const skipPairs = confirm('Пропускати парні?');
     document.location.reload()
-    const result = null;
+    let result = null;
     for(const i = n; i <= m; ++i){
         if(skipPairs && !(i % 2)){
             continue;  
